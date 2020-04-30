e7f95254ef438fcb4890fb9347cc065b408a02b0
H4CKY54CK <tk13xr37@gmail.com>
2020-04-30 15:22:12 -0400

    Better display

--- idkanymore.py
+++ idkanymore.py
@@ -4,15 +4,17 @@ import numpy as np
 from itertools import product
 ROWS = 17
 COLS = 17
+X = '\u2B1B'
+Y = '\u2B1C'
 
-grid = np.array([' ' for i in range(ROWS*COLS)]).reshape(ROWS,COLS)
+grid = np.array([Y for i in range(ROWS*COLS)]).reshape(ROWS,COLS)
 
 # Glider :P
-grid[3,13] = '1'
-grid[4,12] = '1'
-grid[5,12] = '1'
-grid[5,13] = '1'
-grid[5,14] = '1'
+grid[3,13] = X
+grid[4,12] = X
+grid[5,12] = X
+grid[5,13] = X
+grid[5,14] = X
 
 def check(a,b):
     x = [a-1,a,a+1]
@@ -32,7 +34,7 @@ def check(a,b):
 
     box = grid[np.ix_([x[0],x[1],x[2]],[y[0],y[1],y[2]])]
     b = box.ravel()
-    cnt = list(b).count('1')
+    cnt = list(b).count(X)
     return cnt
 
 input("Hit enter to start.")
@@ -41,19 +43,19 @@ while True:
     revive = []
     os.system('cls' if os.name == 'nt' else 'clear')
     for i in grid:
-        print(f"{i}\n")
+        print(''.join(list(i)))
     # print(grid)
     time.sleep(.1)
     for x,y in product(range(ROWS), range(COLS)):
         cnt = check(x,y)
-        if grid[x,y] == '1':
+        if grid[x,y] == X:
             if cnt < 3 or cnt > 4:
                 kill.append((x,y))
-        elif grid[x,y] == ' ':
+        elif grid[x,y] == Y:
             if cnt == 3:
                 revive.append((x,y))
 
     for i in kill:
-        grid[i[0],i[1]] = ' '
+        grid[i[0],i[1]] = Y
     for i in revive:
-        grid[i[0],i[1]] = '1'
+        grid[i[0],i[1]] = X
